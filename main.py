d406137e58ad3195973a1dc81440c253f22797f2
Jack MacKinnon <mackinnonjg@gmail.com>
2026-04-14 22:08:25 -0700

    Added ability to read in GPS lon and lat pairs in main.py

--- main.py
+++ main.py
@@ -9,6 +9,35 @@ vis = GPSVis(data_path='random_coordinates.csv',
 vis.create_image(color=(0, 0, 255), width=3)  # Set the color and the width of the GNSS tracks.
 vis.plot_map(output='save')
 
-
-
 print()
+
+"""
+Writing aircraft GPS locations in the .json file to the .csv file in the format: lat, lon
+
+lastPosition:    if 'lat' and 'lon' are not updated within 60 seconds they are no longer valid.
+However, if the aircraft is still being received, 'lastPosition' will be the GPS coordinates the plane was last seen at.
+"""
+with open('test_info.json', 'r') as jsonfile:   #'r' for "read this file"
+    data = json.load(jsonfile)
+
+rows = []
+
+for aircraft in data['aircraft']:
+    lat = None
+    lon = None
+
+    if 'lat' in aircraft and 'lon' in aircraft:
+        lat = aircraft['lat']
+        lon = aircraft['lon']
+    
+    elif 'lastPosition' in aircraft:    # if 'lat' and 'lon' are not updated in 60 seconds they are no longer valid. If the aircraft is still being received, 'lastPosition' will be the GPS coordinates the plane was last seen at.
+        lat = aircraft['lastPosition'].get('lat')
+        lon = aircraft['lastPosition'].get('lon')
+
+    if lat is not None and lon is not None:
+        rows.append([lat,lon])
+
+    with open('test_coordinates.csv', 'w', newline='') as csvfile:  #'w' for "write to this file"
+        writer = csv.writer(csvfile)
+        writer.writerows(['lat', 'lon'])
+        writer.writerows(rows)
